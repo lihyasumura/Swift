a3e3a0eb55092449689937303e5c737cd1d24c29
Lih Yasumura <97624980+lihyasumura@users.noreply.github.com>
2022-10-26 14:53:16 -0300

    08

---
+++ Day_1/08-Sumary.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var str = "Hello, playground"
+let taylor = "swift"
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var age = 38
+var pi = 3.141
+var awesome = true
+
+var score = 85
+var str2 = "Your score was \(score)"
+
+var album: String = "Reputation"
+var year: Int = 1989
+var height: Double = 1.78
+var taylorRocks: Bool = true
