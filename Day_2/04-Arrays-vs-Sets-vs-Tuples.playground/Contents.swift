82b11fd83a60022a7bc410cb94c5042ca7e2829e
Lih Yasumura <97624980+lihyasumura@users.noreply.github.com>
2022-11-07 09:40:52 -0300

    .

---
+++ Day_2/04-Arrays-vs-Sets-vs-Tuples.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+
+//Valores específicos e fixos - TUPLES
+let address = (house: 555, street: "Taylor Swift Avenue", city: "Nashville")
+
+//Valores únicos ou verificar se um determinado valor está ali rapidamente - SET
+let set = Set(["aardvark", "astronaut", "azalea"])
+
+//Para armazenar itens duplicados ou quando a ordem dos valores importa - ARRAY
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+//Arrays são os mais comumente usados dentre os três tipos
